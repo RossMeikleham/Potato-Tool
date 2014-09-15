bd15c21876a67e0e200274897c667fec87840e7a
RossMeikleham <rossmeikleham@hotmail.co.uk>
2014-09-15 21:11:21 +0100

    Now parses control block of the VMU and deserializes it correctly

--- potato.hs
+++ potato.hs
@@ -3,6 +3,8 @@ import Data.Binary
 import System.IO
 import System.Environment
 import GHC.IO.Handle.FD
+import qualified Data.ByteString.Lazy as BS
+import Data.Bits
 
 {-data Timestamp = 
     { century :: Word8
@@ -16,20 +18,21 @@ import GHC.IO.Handle.FD
     }
 -}
 
-{-data RootBlock =
-    customVMSColor 1/0
-    blueVMS 8
-    redVMS 8
-    greenVMS 8
-    alphaComponent 8
-    timeStamp
-    locationFAT 16
-    sizeFAT 16
-    locationDirectory 16
-    sizeDirectory
-    iconShape 0-123
-    userBlocks 200
--}
+data RootBlock = RootBlock
+    { customVMSColor :: Word8 -- Bool
+    , blueVMS :: Word8
+    , redVMS :: Word8
+    , greenVMS :: Word8
+    , alphaComponent :: Word8
+  --  , timeStamp
+    ,locationFAT :: Word16
+    ,sizeFAT :: Word16
+    ,locationDirectory :: Word16
+    ,sizeDirectory :: Word16
+    ,iconShape :: Word16
+    ,userBlocks :: Word16
+    } deriving Show
+
 data Directory = Directory { entries :: [DirectoryEntry]} deriving Show
 
 
@@ -47,6 +50,43 @@ data FileType = NoFile | Game | Data deriving Show
 
 vmuSize = 128 * 1024 --128KB vmu
 
+slice :: Int -> Int -> [Word8] -> [Word8] 
+slice a b xs = take (b - a + 1) (drop a xs)
+
+-- Work out starting location in file for given block
+blockStart :: Int -> Int
+blockStart b = 512 * b
+
+-- Concatonate two Word8 values into a little
+-- endian Word16
+encodeWord16 :: [Word8] -> Word16
+encodeWord16 (a:b:xs) = a' .|. (b' `shiftL` 8)
+    where a' = fromIntegral a
+          b' = fromIntegral b
+
+-- Obtain the last block Root Block so we can determine
+-- information on the file system to operate on it
+getRootBlock :: [Word8] -> RootBlock
+getRootBlock fileStr = 
+        RootBlock customColor blue red green alpha 
+                  locationFAT sizeFAT locationDir sizeDir iconShape userBlocks
+        
+        where 
+              rootBlockStr = drop (blockStart 255) fileStr
+              customColor = rootBlockStr !! 0x10 -- /= 0x0
+              blue = rootBlockStr !! 0x11
+              green = rootBlockStr !! 0x12
+              red = rootBlockStr !! 0x13
+              alpha = rootBlockStr !! 0x14
+              locationFAT = encodeWord16 $ slice 0x46 0x47 rootBlockStr
+              sizeFAT = encodeWord16 $ slice 0x48 0x49 rootBlockStr
+              locationDir = encodeWord16 $ slice 0x4A 0x4B rootBlockStr
+              sizeDir = encodeWord16 $ slice 0x4C 0x4D rootBlockStr
+              iconShape = encodeWord16 $ slice 0x4E 0x4F rootBlockStr
+              userBlocks = encodeWord16 $ slice 0x50 0x51 rootBlockStr
+
+displayContents :: BS.ByteString -> String
+displayContents bs = show $ getRootBlock $ BS.unpack bs
 
 main :: IO()
 main = do 
@@ -60,9 +100,12 @@ main = do
             else do 
                  file <- openFile vmu ReadMode
                  size <- hFileSize file
+                 hClose file
                  if size /= vmuSize
                      then putStrLn ("VMU is incorrect size (" ++ (show size) ++ " bytes)")
-                     else putStrLn "ok"
+                     else do 
+                            bs <- BS.readFile vmu 
+                            putStrLn $ displayContents bs
                  
                  
                   
